7ea5a54200788571aa44535fa76240c26d9cc1a0
DikshaGunnewar <diksha1995gunnewar2gmail.com>
2017-11-21 18:19:23 +0530

    First Commit

---
+++ src/app/auth/auth0-variables.ts
@@ -0,0 +1,12 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: '59C5u2j0NtoDxAmx1mAkzkJ42meewTzw',
+  domain: 'sociallogin123.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+  
+};
